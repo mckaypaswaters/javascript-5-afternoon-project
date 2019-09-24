f4e95ec06eedba292f66b97ada5dcd499f00855a
McKay Waters <mckaypaswaters@gmail.com>
2019-09-24 16:21:25 -0600

    finished all but #8 on closures

--- closures.js
+++ closures.js
@@ -23,6 +23,8 @@ function outer() {
 */
   
 // Code Here
+let inner = outer()
+inner()
 
 
 
@@ -52,7 +54,8 @@ function callFriend(name) {
 */
 
 //Code Here
-
+let callJake = callFriend('Jake')
+callJake('435-555-9248')
 
 
 ////////// PROBLEM 3 //////////
@@ -62,15 +65,20 @@ function callFriend(name) {
 */
 
 //Code Here
-
+function makeCounter(){
+  let start = 1
+  return function(){
+    return start++
+  }
+}
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -87,17 +95,23 @@ function callFriend(name) {
 
 function counterFactory(value) {
   // Code here.
+  let startVal = value
 
   return {
-
+    inc: function(){
+      return startVal += 1
+    },
+    dec: function(){
+      return startVal -= 1
+    }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -113,9 +127,13 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
+  return function(){
+    let message = `${welcomeText} ${firstname} ${lastname}.`
+    return message
+  }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  // return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,6 +162,9 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: function(){
+      return privateMethod()
+    }
   };
 })();
 
@@ -163,6 +184,12 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret: function(val){
+      return secret += val
+    },
+    takeAwayFromSecret: function(val){
+      return secret -= val
+    }
   };
 }
 
@@ -194,3 +221,13 @@ function timeOutCounter() {
   }
 }
 timeOutCounter();
+
+
+// function timeOutCounter() {
+//   for (var i = 0; i <= 5; i++) {
+//     setTimeout(function() {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+// }
+// timeOutCounter();
