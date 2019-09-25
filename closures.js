72d4353d339a28b1ba32886d8033b50d67a4ca5a
McKay Waters <mckaypaswaters@gmail.com>
2019-09-24 18:28:22 -0600

    finished

--- closures.js
+++ closures.js
@@ -215,9 +215,12 @@ function secretNumber() {
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+    function pizza(index){
+      setTimeout(() => {
+        console.log(index);
+      }, i * 1000);
+    }
+    pizza(i)
   }
 }
 timeOutCounter();
